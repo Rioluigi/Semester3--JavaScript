0fb50b4c6603a84145021d49861f36114e25367b
Rioluigi <113849494+Rioluigi@users.noreply.github.com>
2023-10-05 22:09:12 +0700

    Add files via upload

---
+++ sesi 2/Pemograman Berbasis Platfom_Sesi2_Rio.js
@@ -0,0 +1,13 @@
+var namaLengkap = "Rio Luigi Del Niery"
+var namaPanggilan = "Iyo"
+var asalKampus = "Universitas Nusa Putra"
+var asalProdi = "Program Studi Teknik Informatika"
+var usia = 19
+var isMarried = false
+
+console.log(namaLengkap);
+console.log(namaPanggilan);
+console.log(asalKampus);
+console.log(asalProdi);
+console.log ("Usia:",usia);
+console.log("isMarried:", isMarried);
