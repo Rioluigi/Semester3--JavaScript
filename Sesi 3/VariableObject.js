7fba43d9b6c74aeaf471603554bb58645981d568
Rioluigi <113849494+Rioluigi@users.noreply.github.com>
2023-10-20 17:03:35 +0700

    Add files via upload

---
+++ Sesi 3/VariableObject.js
@@ -0,0 +1,18 @@
+// Nomor 1
+console.log("Tugas Nomor 1 Variable Bertipe Objek")
+
+const properti1 = { 
+ Nama : "Rio Seafood",
+ City : "Cibanceuy",
+ favoriteDrink :"Es Kuwut, Es teh Manis",
+ favoriteFood: "Kepiting Alaska, Cumi S.Tiram, Udang Goreng Tepung",
+ isVegan : true
+
+}
+console.log(properti1)
+
+// Nomor 2
+console.log("Tugas 3 Nomor 2 Variable Bertipe array")
+
+let restaurant = ["Rio Seafood","Cibanceuy","Es Kuwut, Es teh Manis","Kepiting Alaska, Cumi S.Tiram, Udang Goreng Tepung",true]
+console.log(restaurant)
